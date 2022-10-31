bd385de21d1a837437a6ceec50832f4fb68180cd
Дима <@DESKTOP-TC925PR>
2022-10-31 19:17:19 +0200

    Add project files.

---
+++ 17.10/17.10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include "directori.h"
+using namespace std;
+
+int main() {
+	Directory obj("zemlya", "Dima", "0503950580", "Odessa", "C++");
+	obj.Input();
+	obj.Save();
+	obj.Print();
+
+	Directory obj2("Bus", "Villa", "3023123930549", "Kubey", "Python");
+	obj2.Input();
+	obj2.Save();
+	obj2.Print();
+
+}
